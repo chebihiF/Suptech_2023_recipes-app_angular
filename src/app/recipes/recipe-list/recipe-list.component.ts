16f326f7db35f35bc448b8905b8e885509453f48
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-04-08 13:22:54 +0000

    first commmit 08-04-2023

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe', 'This is a simple test', 
+    'https://img.buzzfeed.com/video-api-prod/assets/eb44570519264864814264f7f0a5e47a/BFV13909_BakedRatatouille-ThumbTextless1080.jpg?resize=1200:*'),
+    new Recipe('A New test Recipe', 'This is a simple test', 
+    'https://www.seriouseats.com/thmb/UwrB6s6MgT_P1XmQPiMyUXWgWWc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/classic-new-orleans-muffuletta-recipe-Hero1-ee95576a16584f298d6abe7fac80712b.jpg')
+ 
+  ]
+
+  constructor(){}
 
 }
